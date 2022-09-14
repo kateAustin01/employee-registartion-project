96a3458d12d767eb10ec73b7c3263c12efa3e85e
aissakhova@gmail.com <aktoty@live.com>
2022-09-13 22:44:03 -0500

    Initial commit

---
+++ src/main/java/com/cydeo/employeeregistartionproject/EmployeeRegistartionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistartionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistartionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistartionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cydeo/employeeregistartionproject/EmployeeRegistartionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistartionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistartionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
