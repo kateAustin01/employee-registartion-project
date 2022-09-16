32348d6778770a05c76f8bb707d7c0ee1cd7188c
aissakhova@gmail.com <aktoty@live.com>
2022-09-16 13:22:53 -0500

    Validation messages addded

--- src/main/java/com/cydeo/employeeregistartionproject/controller/EmployeeController.java
+++ src/main/java/com/cydeo/employeeregistartionproject/controller/EmployeeController.java
@@ -4,10 +4,13 @@ import com.cydeo.employeeregistartionproject.bootstrap.DataGenerator;
 import com.cydeo.employeeregistartionproject.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -15,17 +18,25 @@ public class EmployeeController {
     @GetMapping("/register")
     public String createEmployee(Model model) {
 
-        model.addAttribute("employee",new Employee());
+        model.addAttribute("employee", new Employee());
         model.addAttribute("states", DataGenerator.getAllStates());
         return "employee/employee-create";
 
     }
 
-    @PostMapping ("/list")
-    public String employeeList(@ModelAttribute("employee")Employee employee,Model model) {
 
-DataGenerator.saveEmployee(employee);
-model.addAttribute("employees",DataGenerator.readEmployees());
+    //Binding result should be put before Model model
+    @PostMapping("/list")
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("states", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
+
+        DataGenerator.saveEmployee(employee);
+        model.addAttribute("employees", DataGenerator.readEmployees());
 
         return "employee/employee-list";
 
--- src/main/java/com/cydeo/employeeregistartionproject/model/Employee.java
+++ src/main/java/com/cydeo/employeeregistartionproject/model/Employee.java
@@ -5,6 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.cglib.core.Local;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -12,9 +19,29 @@ import org.springframework.cglib.core.Local;
 @NoArgsConstructor
 public class Employee {
 
+    /*
+*
+* An empty string is a String object with an assigned value, but its length is equal to zero.
+A null string has no value at all.
+A blank String contains only whitespaces, are is neither empty nor null, since it does have an assigned value, and isn't of 0 length.
+String nullString = null;
+String emptyString = "";
+String blankString = " ";
+    *
+    *
+    * */
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(max = 12,min = 2)
     private String firstName;
     private String lastName;
-    private Local birthday;
+
+
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
     private String email;
     private String password;
     private String address;
